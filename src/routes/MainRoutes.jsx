be73b89739a072ddb27e12fda665bc77d960b2d4
Liraamm <ughmyname18@gmail.com>
2023-07-18 15:44:32 +0600

    first-commit

---
+++ src/routes/MainRoutes.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import HomePage from "../pages/HomePage";
+import MoviesPage from "../pages/MoviesPage";
+import AddMoviePage from "../pages/AddMoviePage";
+import NotFoundPage from "../pages/NotFoundPage";
+
+const MainRoutes = () => {
+  return (
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/add" element={<AddMoviePage />} />
+      </Route>
+      <Route path="*" element={<NotFoundPage />} />
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
+};
+
+export default MainRoutes;
